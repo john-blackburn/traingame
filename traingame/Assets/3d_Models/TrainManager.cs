643d474c8c32e554f6bf9238c7b54c28e3cbe6ef
John Blackburn <john.blackburn75@gmail.com>
2015-09-01 00:12:08 +0100

    Add train assets provided by Bo

---
+++ traingame/Assets/3d_Models/TrainManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrainManager : MonoBehaviour {
+
+	// this is just a test
+     [Header("Train Dimensions")]
+	public float Tr_width;
+	public float Tr_Length;
+	BoxCollider _trCollider;
+	// Use this for initialization
+	void Start () {
+	
+		_trCollider = transform.GetComponent<BoxCollider>() as BoxCollider;
+		Debug.Log("Length : "+_trCollider.bounds.size.z);
+		Debug.Log("width : "+_trCollider.bounds.size.x);
+
+	}
+	
+
+}
