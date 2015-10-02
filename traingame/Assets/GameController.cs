7b70ad60664644e266af3c897acc81f6202f48ba
John Blackburn <john.blackburn75@gmail.com>
2015-10-02 21:52:47 +0100

    Simple board train with jump scare!
    Testing Coroutines. Seems a good way to do sequential animations.

---
+++ traingame/Assets/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject TrainPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+		GameObject train = (GameObject)Instantiate (TrainPrefab);
+		train.transform.position = new Vector3 (0f, 0f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
