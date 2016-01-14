4f93165de811eb0e8b3294307b18bb6e80bce4c3
John Blackburn <john.blackburn75@gmail.com>
2016-01-14 01:55:43 +0000

    Test boarding train and departing

--- traingame/Assets/TunnelsScripts/TunnelGameController.cs
+++ traingame/Assets/TunnelsScripts/TunnelGameController.cs
@@ -10,7 +10,8 @@ public class TunnelGameController : MonoBehaviour {
 	private float tunnelLength, tunnelWidth, stationLength, stationWidth;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		mainCamera = GameObject.FindWithTag ("MainCamera");
 
 		GameObject tunnelSection;
@@ -60,20 +61,92 @@ public class TunnelGameController : MonoBehaviour {
 //		foreach (Transform child in train.transform) {
 //			print (child);
 //		}
+		StartCoroutine(playGame());
+	}
 
-		StartCoroutine (trainArrives());
+	IEnumerator playGame()
+	{
+		yield return StartCoroutine (trainArrives());
 
+		yield return StartCoroutine(rotateCameraTo (0, 180, 0, 20));
+		yield return StartCoroutine(moveCameraTo (0, 0, 0, 60));
+		yield return StartCoroutine(rotateCameraTo (0, -90, 0, 20));
+		yield return StartCoroutine(moveCameraTo (-2, 0, 0, 40));
+		yield return StartCoroutine(rotateCameraTo (0, 0, 0, 20));
 
+		mainCamera.transform.parent = train.transform;
+		yield return StartCoroutine (trainLeaves ());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	}
 
+	// ###########################################################
+	
+	float inOutExponential(float ratio)
+	{
+		if (ratio == 0 || ratio == 1) {
+			return ratio;
+		}
+		ratio = ratio*2-1;
+		if (ratio<0) {
+			return 0.5f*Mathf.Pow(2, 10*ratio);
+		}
+		return 1-0.5f*Mathf.Pow(2, -10*ratio);
+	}
+
+	// ###########################################################
+	
+	IEnumerator moveCameraTo (float x, float y, float z, int nframes, bool relative=false)
+	{
+		Vector3 r1 = mainCamera.transform.position;
+		Vector3 r2; 
+		
+		if (relative)
+			r2 = r1 + new Vector3 (x, y, z);
+		else
+			r2 = new Vector3 (x, y, z);
+		
+		float ds = 1.0f / nframes;
+		for (int i=0; i<=nframes; i++) {
+			mainCamera.transform.position = r1 + (r2 - r1) * i * ds;
+			yield return null;
+		}
+		
+	}
+
+	IEnumerator rotateCameraTo(float x, float y, float z, int nframes)
+	{
+		Quaternion q1 = mainCamera.transform.rotation;
+		Quaternion q2 = Quaternion.Euler (x,y,z);
+		float s;
+
+		float ds = 1.0f / nframes;
+		for (int i=0;i<=nframes;i++) {
+			mainCamera.transform.rotation = Quaternion.Slerp (q1, q2, i*ds);
+			yield return null;
+		}
+	}
+
+
 	IEnumerator trainArrives()
 	{
-		for (int i=1000; i>=0; i--) {
-			Vector3 pos=new Vector3(i,0,0);
+		float s;
+
+		for (s=0; s<1; s+=0.01f) {
+			Vector3 pos=new Vector3(500-500*inOutExponential(s),0,0);
+			train.transform.position=pos;
+			yield return null;
+		}
+	}
+
+	IEnumerator trainLeaves()
+	{
+		float s;
+		
+		for (s=0; s<1; s+=0.002f) {
+			Vector3 pos=new Vector3(200*inOutExponential(s),0,0);
 			train.transform.position=pos;
 			yield return null;
 		}
