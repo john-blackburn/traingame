8adb7d4a3dc469d49a73ddbb98796a0a01396dc3
John Blackburn <john.blackburn75@gmail.com>
2016-01-15 18:17:13 +0000

    Demo showing potentially infinitely long tunnel between stations

--- traingame/Assets/TunnelsScripts/TunnelGameController.cs
+++ traingame/Assets/TunnelsScripts/TunnelGameController.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Note: anchor point for train is front of first carriage (direction of travel = +x)
+// anchor point for for tunnels is rear of tunnels (made of tunnel sections)
+// anchor point for station is at centre of station (but note offset in y,z)
+
 public class TunnelGameController : MonoBehaviour {
 
 	public GameObject TrainCabinPrefab,TunnelSectionPrefab,StationPrefab;
@@ -39,7 +43,7 @@ public class TunnelGameController : MonoBehaviour {
 
 		for (int i=0; i<3; i++) {
 			GameObject trainCabin = Instantiate (TrainCabinPrefab) as GameObject;
-			trainCabin.transform.localPosition = new Vector3(tunnelLength*i/2,0,0);
+			trainCabin.transform.localPosition = new Vector3(-tunnelLength/4-tunnelLength*i/2,0,0);
 			trainCabin.transform.parent = train.transform;
 		}
 
@@ -49,12 +53,12 @@ public class TunnelGameController : MonoBehaviour {
 		print (train.transform.childCount);
 
 		tunnel = new GameObject();
-		tunnel.transform.position = new Vector3 (stationLength, 0, 0);
+		tunnel.transform.position = new Vector3 (stationLength/2, 0, 0);
 
 		for (int i=0; i<3; i++) {
 			GameObject t=Instantiate(TunnelSectionPrefab) as GameObject;
 			t.transform.parent=tunnel.transform;
-			t.transform.localPosition=new Vector3(tunnelLength*(i-0.5f),0,0);
+			t.transform.localPosition=new Vector3(tunnelLength*(i+0.5f),0,0);
 		}
 
 
@@ -64,19 +68,92 @@ public class TunnelGameController : MonoBehaviour {
 		StartCoroutine(playGame());
 	}
 
+	// ###########################################################
+
 	IEnumerator playGame()
 	{
-		yield return StartCoroutine (trainArrives());
+		yield return StartCoroutine (trainArrives ());
 
-		yield return StartCoroutine(rotateCameraTo (0, 180, 0, 20));
-		yield return StartCoroutine(moveCameraTo (0, 0, 0, 60));
-		yield return StartCoroutine(rotateCameraTo (0, -90, 0, 20));
-		yield return StartCoroutine(moveCameraTo (-2, 0, 0, 40));
-		yield return StartCoroutine(rotateCameraTo (0, 0, 0, 20));
+		yield return StartCoroutine (rotateCameraTo (0, 180, 0, 20));
+		yield return StartCoroutine (moveCameraTo (0, 0, 0, 60));
+		yield return StartCoroutine (rotateCameraTo (0, -90, 0, 20));
+		yield return StartCoroutine (moveCameraTo (-2, 0, 0, 40));
+		yield return StartCoroutine (rotateCameraTo (0, 180, 0, 20));
 
 		mainCamera.transform.parent = train.transform;
-		yield return StartCoroutine (trainLeaves ());
+
+		float xtun0 = tunnel.transform.position.x;   // starting position of tunnel back-end
+
+		int i,nacc,n,nmore;
+		float x,a,x0,x1,u;
+
+		a = 0.01f;       // acceleration
+		u = 1f;          // top speed  (m/frame)
+		n = 143;         // no of const speed frames
+		nmore = 6;       // additional tunnel sections for slow down
+
+		nacc = (int)(u/a);   // no time steps to get to max speed
+
+		print ("accelerate");
+		for (i=0; i<nacc; i++) {
+			x = 0.5f * a * i * i;
+			train.transform.position = new Vector3 (x, 0, 0);
+			cycleTunnel(x);
+			yield return null;
+		}
+
+		x0 = 0.5f * a * nacc * nacc;   // distance covered in acc'n phase
+		print ("const speed");
+		for (i=0; i<n; i++) {
+			x = x0 + i * u;
+			train.transform.position = new Vector3 (x, 0, 0);
+			cycleTunnel(x);
+			yield return null;
+		}
+
+		int sectionsDone;
+		float totLength,distRemain;
+
+		x1 = x0 + n * u;
+		sectionsDone = (int)((x1 - xtun0) / tunnelLength);
+		totLength = (sectionsDone + nmore) * tunnelLength;
+		distRemain = totLength - (x1 - xtun0) + stationLength / 2;
+		a = -u * u / (2 * distRemain);
+
+		print ("sectionsDone"+sectionsDone);
+
+		station.transform.Translate (new Vector3 (stationLength + totLength, 0, 0));
+
+		print ("slow down" + a);
+		for (i=0; i<=(int)(-u/a); i++) {
+			x = x1 + u * i + 0.5f * a * i * i;
+			train.transform.position = new Vector3 (x, 0, 0);
+			cycleTunnel (x);
+			yield return null;
+		}
+
+//		yield return StartCoroutine (moveTrainTo (stationLength + 3 * tunnelLength, 0, 0, 200, 0, 0.5f));
+//		station.transform.position = new Vector3 (stationLength + 3 * tunnelLength, -3, -1.6f);
+//		yield return StartCoroutine (moveTrainTo (stationLength + 3 * tunnelLength, 0, 0, 200, 0.5f, 1));
+
+//		tunnel.transform.position = new Vector3 (1.5f * stationLength + 3 * tunnelLength, 0, 0);
 	}
+
+	void cycleTunnel(float x)   // x: train front position
+	{
+		float xtunnel = tunnel.transform.position.x;
+
+		if (x > xtunnel + 2 * tunnelLength) {
+			float xend = xtunnel + 4 * tunnelLength;   // end point of tunnel if we move it
+			float xstation = station.transform.position.x;
+//			print (xend+":"+xstation+":"+stationLength);
+			if (!(xend > xstation - stationLength / 2.1 && xend < xstation + stationLength / 2.1))
+				tunnel.transform.Translate (new Vector3 (tunnelLength, 0, 0));
+		}
+
+	}
+
+	// ###########################################################
 	
 	// Update is called once per frame
 	void Update () {
@@ -116,6 +193,8 @@ public class TunnelGameController : MonoBehaviour {
 		
 	}
 
+	// ###########################################################
+
 	IEnumerator rotateCameraTo(float x, float y, float z, int nframes)
 	{
 		Quaternion q1 = mainCamera.transform.rotation;
@@ -129,24 +208,47 @@ public class TunnelGameController : MonoBehaviour {
 		}
 	}
 
+	// ###########################################################
+
+	IEnumerator moveTrainTo(float x, float y, float z, int nframes, float s1=0f, float s2=1f)
+	{
+		// Move train from current point to (x,y,z) in nframes.
+		// if s1=0 and s2=1, do whole animation
+		// else do partial animation [s1,s2]
+
+		Vector3 r1 = mainCamera.transform.position;
+		Vector3 r2 = new Vector3 (x, y, z);
+		
+		float ds = (s2-s1) / nframes;
+
+		for (int i=0; i<nframes; i++) {
+			Vector3 pos = r1 + (r2 - r1) * inOutExponential (s1 + i * ds);
+			train.transform.position = pos;
+			yield return null;
+		}
+	}
+
+	// ###########################################################
 
 	IEnumerator trainArrives()
 	{
 		float s;
 
 		for (s=0; s<1; s+=0.01f) {
-			Vector3 pos=new Vector3(500-500*inOutExponential(s),0,0);
+			Vector3 pos=new Vector3(520-500*inOutExponential(s),0,0);
 			train.transform.position=pos;
 			yield return null;
 		}
 	}
+
+	// ###########################################################
 
 	IEnumerator trainLeaves()
 	{
 		float s;
 		
 		for (s=0; s<1; s+=0.002f) {
-			Vector3 pos=new Vector3(200*inOutExponential(s),0,0);
+			Vector3 pos=new Vector3((stationLength+3*tunnelLength)*inOutExponential(s),0,0);
 			train.transform.position=pos;
 			yield return null;
 		}
